14e29f529af43c1c876ec0ac07a93df167398550
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-08 13:19:09 -0700

    Hello,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,7 @@
 /**
 * main - print the string in the put function
 *
-* Description: using the main function
-* this program prints "programming is like building a multilingual puzzle
-* Return: 0
+* Return: 0 Always (Success)
 */
 int main(void)
 {printf("with proper grammar, but the outcome is piece of art,\n");
