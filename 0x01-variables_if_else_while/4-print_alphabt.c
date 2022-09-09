72d0994f941c9bd48195da62c970cbc9484432bb
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-09 12:54:16 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print if the number if positive, zero, or negative
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if(c != 'e' && c !='q'
+				{
+					putchar(c);
+				}
+				}
+				putchar(\n');
+				return (0)
+				}
