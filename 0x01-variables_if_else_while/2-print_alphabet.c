af6dd963d2eafd82b209b26889262d8bbb568c1e
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-10 06:09:56 -0700

    TASK-2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,11 +9,13 @@
 int main(void)
 
 {
-char ch;
-for (ch - 'a' ; ch <- 'z' ; ch++)
-{
-	putchar(ch);
-}
-putchar('\n');
-return (0);
+	char alphabet;
+	for  (alphabet = 'a'; alphbet <= 'z'; alphabet)
+	{
+		putchar(alphabet);
+	}
+
+	putchar("\n");
+	return (0);
+
 }
