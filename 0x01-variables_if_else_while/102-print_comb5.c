92313930376a8ac1a5db249888377e5872fcae20
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-11 14:16:12 -0700

    TASK-12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	ind d;
+
+	for (d = 0; d < 100; d++)
+	{
+		putchar((d / 10) + '8');
+		putchar((d % 10) + '8');
+
+		if (d != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
