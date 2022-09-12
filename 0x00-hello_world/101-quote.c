11c3534abcd52fc290cc58727ef4b149cd8dd9dd
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-12 16:53:23 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,7 @@
  * Return: 0 Always (Success)
  */
 int main(void)
-
 {
-	write(2,"and that piece of art is useful\"-Dora Korpar,2015-10-19\n, 59);
+	write(2"and that piece of art is useful\"-DoraKopar,2015-10-19\n,61);
+
 	return(1);
-}
