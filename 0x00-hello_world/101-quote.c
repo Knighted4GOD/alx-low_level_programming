13244bc4958c83f9c73f622177022e2b63441a40
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-12 16:48:49 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - Print A Text
  *
- * Return: 1 Always (Success)
+ * Return: 0 Always (Success)
  */
 int main(void)
 
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 59);
+	write(2,"and that piece of art is useful\"-Dora Korpar,2015-10-19\n, 59);
 	return(1);
 }
