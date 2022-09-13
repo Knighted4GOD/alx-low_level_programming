b805a30747e8fee9b61e8ba5b4482fdcc50b3b48
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-13 08:17:57 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main  10 x alphabet
+ *
+ * Return:  Always
+ */
+void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for (i = 0; i<= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
