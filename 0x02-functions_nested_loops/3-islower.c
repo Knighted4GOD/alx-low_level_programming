df9f58c4ffe3469e91da62d2d96faaa0bf29adae
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-13 08:37:41 -0700

    TASK-3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - Function to check for lowercase figures
+ *
+ * Return: 0 Always
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		retur (0);
+}
+
