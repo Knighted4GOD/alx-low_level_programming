d4f144e953916a8f48dae34642606def58d2f5bb
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-13 09:39:30 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha - code for checking alphabetical figures
+ * @c: figures to be checked on
+ *
+ * Return: 0 or 1 base of terms
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && C >= 'Z'));
+}
