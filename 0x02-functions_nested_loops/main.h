2f947f30e21507ae3b03634e7c42d2584606e1d6
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-13 07:30:02 -0700

    0. _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,17 @@
 #ifndef HOLBERTON_H_INCLUDE
-#define HOLBERTON_H_INCLUDE
+#defind HOLBERTON_H_INCLUDE
 
-int _putchar(char);
+#define HOLBERTON_H
+
+void print_alphabet_x10(void);
 void print_alphabet(void);
-void print_alphabet_x10(void)
+void print_alphabet_
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int);
+void print_to_98(int n);
+#endif
 void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
