6c8d22c8701ca79b114c2be28c3e2f2de1a1f4d6
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-12 22:43:19 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,10 @@
 /**
  * main - Print A Text
  *
- * Return: 0 Always (Success)
+ * Return: 1 Always (Success)
  */
 int main(void)
+
 {
 	write(2, "andthatpieceofartisuseful\"-DoraKopar,2015-10-19\n" ,59);
 	return(1);
