82a856dfa3b26996035aa07d62d29e677756fe1a
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-14 12:18:34 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,10 +3,12 @@
 /**
  * jack_bauer - function that prints every munite of the day of Jack Bauer
  * h = hour, m = minutes
+ *
  * Return: 0
  */
 void jack_bauer(void)
 {
+
 	int h, m;
 	for (h = 0; h < 24; h++)
 	{
