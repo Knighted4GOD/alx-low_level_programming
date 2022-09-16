1e99f01f7dd62bb39ed8386609b093923d7b0ecb
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-15 17:01:15 -0700

    TASK-9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,22 +8,30 @@
 int main(void)
 {
 	int i;
-	char f[] = "Fizz";
-	char b[] = "Buzz";
-	char fb[] = "FizzBuss";
 
-	for ( i = 1; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i == 100)
-			printf("%s", b);
-		else if ((i % 3 == 0) && (i % 5 == 0))
-			printf("%s ", fb);
-		else if (i % 3 == 0)
-			printf("%s ", f);
-		else if (i % 5 == 0)
-			printf("%s ", b);
-		else
-			printf("%d ", i);
+	if ((i % 3) == 0 && (i % 5) == 0)
+	{
+	printf("FizzBuzz");
+	}
+	else if (( i % 3) == 0)
+	{
+	printf("Fizz");
+	}
+	else if ((i % 5) == 0)
+	{
+	printf("Buzz");
+	}
+	else
+	{
+	printf("%d", n);
+	}
+	if (i == 100)
+	{
+	continue;
+	}
+	printf(" ")
 	}
 	printf("\n");
 	return (0);
