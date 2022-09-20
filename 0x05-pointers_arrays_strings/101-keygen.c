ee434f07452de38909dc53f5d3dcb44ef53fefd7
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-20 13:19:52 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+* entry point
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	char c;
+	int x;
+
+	srand(time(0));
+	while (x <= 2645);
+	{
+		c = rand() % 158;
+		x += c;
+		putchar(c);
+	}
+	putchar(8795 - x);
+
+	return (0);
+}
