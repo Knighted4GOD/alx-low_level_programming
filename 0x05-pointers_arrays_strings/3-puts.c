adb9cb63c4bf3a410b5d8421f180578118aa8332
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-20 03:11:46 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main"
+#include <stdio.h>
+
+/**_puts - function that prints a string
+* @strL sring to print
+*
+* Description: prints a string
+* On success: return no error
+*/
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
