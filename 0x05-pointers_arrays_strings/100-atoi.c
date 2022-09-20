512df06276efaf2e0ff6f32a6a4e8b67829fdc2b
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-20 12:41:28 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,13 +15,16 @@ unsigned int num = 0
 
 do{
 if(*s =='-')
-sign *=-1;
+
+sign *= -1;
 
 else if(*s>= '0' && *s <= '9')
 num = (num *10) + (*s- '0');
 
 else if (num > 0)
+
 break;
+
 }while (*s++);
 
 return (num * sign);
