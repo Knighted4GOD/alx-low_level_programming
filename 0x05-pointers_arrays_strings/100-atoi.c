e8c39c334f86f8b3429776e0c1221e3c0daf036e
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-20 13:10:50 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,22 +10,20 @@
 
 int _atoi(char *s)
 {
+	int sign = 1;
+	unsigned int num = 0;
 
-unsigned int num = 0;
+	do {
+		if (*s == '-')
+			sign *= -1;
 
-do{
-if(*s =='-')
+		else if (*s >= '0' && *s <= '9')
+			num = (num *10) + (*s- '0');
 
-sign *= -1;
+		else if (num > 0)
+			break;
 
-else if(*s>= '0' && *s <= '9')
-num = (num *10) + (*s- '0');
+	} while (*s++);
 
-else if (num > 0)
-
-break;
-
-}while (*s++);
-
-return (num * sign);
+	return (num * sign);
 }
