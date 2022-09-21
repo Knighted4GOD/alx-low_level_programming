f110b03d94488e54969b7188bfb3b324c6971a78
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-21 06:22:17 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* rot13 - rot13 encoder
+* @str: string to be encoded
+*
+* Return: address of the string being coding
+*/
+
+char *rot13(char *)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		str[i] = transfor_2(str[i]);
+		i++;
+	}
+	return (str);
+}
