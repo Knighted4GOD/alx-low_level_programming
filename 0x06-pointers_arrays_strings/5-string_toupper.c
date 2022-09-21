7b025f69474659fb2bebf811a91885e3ab9ee835
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-21 01:35:37 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*string_toupper -  function that changes all lowercase letters of a string to uppercase.
+*@str: Param
+*Return; Always 0
+*/
+
+char *string_toupper(char *)
+{
+	int index = 0;
+
+	while (str[index++])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+	}
+
+	return (str);
+}
