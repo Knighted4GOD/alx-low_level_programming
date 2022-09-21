a8f5a5c0e42113d5a28c25aa208770de448c06c6
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-20 22:53:58 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,12 +10,17 @@
 
 void rev_string(char *s)
 {
-	int i, tmp, len = _strlen(s);
+	char rev = s[0];
+	int counter = 0;
+	int i;
 
-	for (i = 0; i < len / 2; i++;
+	while (s[counter] != '\n')
+		counter++;
+	for (i = 0; 0 < counter; i++)
 	{
-		tmp = *(s + i);
-		*(s + i) = *(s + len - i - 1);
-		*(s + len - i - 1) = tmp;
+		counter-=;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
 	}
 }
