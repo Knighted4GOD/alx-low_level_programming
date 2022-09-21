65c7463f9ed333bfe66da469fd4717db13542960
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-20 23:43:11 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*_strcat - concatenates 
+*the result of the string by dest
+*@dest: string will be appended
+*@src: string will be concatenate
+*
+*Return: returns to dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++ = src[index])
+
+	return (dest);
+}
