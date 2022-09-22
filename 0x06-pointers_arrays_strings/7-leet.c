745d0613039afe3e2f32da2d9887707856e96e0c
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-22 15:05:29 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -23,29 +23,3 @@ char *leet(char *s)
 	}
 	return (s);
 }
-
-/**
- * tranform - helper function to map a letter with it's leet encoding
- * @x: char to be encoded char
-
- *Return: encoded  char
- */
-
-char tranform(char x)
-{
-	char mapping_low[8] = {'o', 'l', '\0', 'e', 'a', '\0', '\0', 't'};
-	char mapping_upper[8] = {'O', 'L', '\0', 'E', 'A', '\0', '\0', 'T'};
-	int i = 0;
-	char replacement = x;
-
-	while (i < 8)
-	{
-		if (x == mapping_low[i] || x == mapping_upper[i])
-		{
-			replacement = i + '0';
-			break;
-		}
-		i++;
-	}
-	return (replacement);
-}
