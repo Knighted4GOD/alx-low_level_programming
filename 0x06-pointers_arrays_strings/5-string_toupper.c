217ae1e01e5405c7a9c5f24618a77d73086e45b7
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-22 14:53:44 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*string_toupper -  function that changes all lowercase letters of a string to uppercase.
+*string_toupper - all lowercase letters of a string to uppercase.
 *@str: Parameter
 *Return: returnss a character
 */
