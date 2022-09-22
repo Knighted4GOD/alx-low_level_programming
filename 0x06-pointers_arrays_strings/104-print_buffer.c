fb42d4a9239818054f69b6e0099653af298cfa1a
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-22 15:37:58 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
-* print_buffer - prints bytes of buffer
+* print_line - prints an s bytes of buffer
 * @c: buffer to be printed
 * @s: bytes of buffer that will be printed
 * @l: line of buffer that must be printed
@@ -10,7 +10,7 @@
 * Return: Always 0
 */
 
-void print_buffer(char *b, int size)
+void print_line(char *c, int s, int i)
 {
 	int j, k;
 
@@ -28,6 +28,35 @@ void print_buffer(char *b, int size)
 		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
 			putchar(c[l * 10 + k]);
 		else
-			putchar(' ');
+			putchar('.');
 	}
 }
+
+/**
+ * prints_buffer - prints a buffer
+ * @b: buffer to be print
+ * @size: size of buffer
+ *
+ * Return: always 0
+ */
+void print_buffer(char *b, int size)
+{
+	int i;
+
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	{
+		printf("%d08x: ", i * 10);
+		if (i < size / 10)
+		{
+			print_line(b, 9, i);
+		}
+		else
+		{
+			printf_line(b, size % 10 - 1, i);
+		}
+		putchar('\n');
+	}
+	if (size == 0)
+		putchar('\n');
+}
+
