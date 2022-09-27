38a4cf4f564dae5770229ffac6f1af621e7eea1e
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-26 20:13:14 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_memset -  function that fills memory with a constant byte.
+* @s: to memory block
+* @b: chat to be used
+* @n: num of bytes that will be used
+*
+*Return; Always 0
+*/
+
+{
+while (n)
+{
+s[n - 1] = b;
+n--;
+}
+return (s);
+}
