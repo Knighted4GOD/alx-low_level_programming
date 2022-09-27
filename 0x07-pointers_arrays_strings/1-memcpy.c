aa45b9ea4530a2b1f0bb58aed6536734b5da3560
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-26 20:49:47 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#inlude "main.h"
+
+/**
+* _memcpy
+* @dest: memory area
+* @src: memory from
+* @n: num of bytes to be copied
+
+* Return; Always 0
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned in i = 0;
+
+	for (; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
