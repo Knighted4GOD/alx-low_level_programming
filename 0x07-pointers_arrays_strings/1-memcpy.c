145bc36d4e0e497f6b2757c7b4a7fde99b5c6f9f
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-27 10:19:58 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,11 +11,12 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned in i = 0;
+	unsigned int i = 0;
 
-	for (i = 0; i < n; i++)
+	for (; i < n; i++)
+	{
 		dest[i] = src[i];
-	
+	}
 
 	return (dest);
 }
