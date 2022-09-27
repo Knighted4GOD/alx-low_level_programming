13028053c43f7be549a8ce4d146a49012237488a
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-27 09:05:59 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,18 +10,18 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i = 0;
-	int j o 0;
-
+	int j = 0;
+			
 	for (; s[i]; i++)
 	{
 		for (j = 0; accept[j]; j++)
 		{
-			if (s[i] == accept[j])
+			if (s[i]==accept[j])
 			{
 				break;
 			}
 		}
-			if (s[i] !=accept[j]
+			if (s[i] != accept[j])
 			break;
 	}
 	return (i);
