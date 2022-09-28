ecbb3e242c99c470129a73f62a45408abc7e1cff
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-28 08:45:08 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _puts_recursion -  function that prints string followed by a new line
+* @s string to be printed
+*
+* Return: 0
+*/
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n'');
+	}
+	else
+	{
+		_putchar(s[o]);
+		_puts_recursion(s +1);
+	}
