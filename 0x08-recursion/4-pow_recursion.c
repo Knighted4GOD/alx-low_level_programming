db18ed6adb41b4dd0e09a996cf4cd26b69167df2
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-28 12:04:22 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _pow_recursion - function that returns the value of x raised to the power of y
+* _pow_recursion - returns the value of x raised to the power of y
 * @x: num to be used
 * @y: the power
 * Return: 0
