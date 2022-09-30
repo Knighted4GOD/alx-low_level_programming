af713d3c56554199b31c11c97ca99a4e7f00d0e8
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-30 10:28:09 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 			str = argv[i];
 			for (b = 0; str[b] != '\0'; b++)
 			{
-				if (str [b] < 48 || str[b] > 57)
+				if (str[b] < 48 || str[b] > 57)
 				{
 					printf("error\n");
 					return (1);
