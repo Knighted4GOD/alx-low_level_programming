a98130f2b608bfc1a76a7135b65e9c1b45f638f8
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-30 09:23:44 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function that print the name of the program
+ * @argc: Argument Count
+ * @argv: Argument Value
+ * 
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
