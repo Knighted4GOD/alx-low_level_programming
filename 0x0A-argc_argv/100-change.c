8cb4995a9ea48dff3bafd163e0094d2ff77fdd2a
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-30 10:52:26 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,7 +9,7 @@
  * Rerurn: 0
  */
 
-int main(int argc, char *argv[])
+int main(int args, char *argv[])
 {
 	int c, coins = 0;
 
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 	{
 		if (c >= 25)
 			c -= 25;
-	
+
 		else if (c >= 10)
 			c -= 10;
 
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
 
 		else if (c >= 2)
 			c -= 2;
-		
+
 		else if (c >= 1)
 			c -= 1;
 		else
