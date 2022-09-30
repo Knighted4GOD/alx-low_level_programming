62dcb0bde649767741ea6c10d6219e8e82f73211
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-30 09:51:20 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - funtion to print name of program
+ * @argc: Argument Count
+ * @argv: Argument Value
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int count = 0;
+
+	if (argc > 0)
+	{
+		while (count <argc)
+		printf("%s\n", argv[count]);
+	}
+}
