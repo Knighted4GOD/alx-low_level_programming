f3af86a77e3a41773eb84cab3aa4d7196dee93fb
Abraham Knight <Knighted4GOD@gmail.com>
2022-09-30 09:39:12 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function that prints the name of the program
+ * @argc: Argument Count
+ * @argv; Argument Value
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv; /*ignore argv*/
+	printf("%i\n", argc - 1);
+
+	return (0);
+}
