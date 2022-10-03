63a71dde840879b1825d22a7413cb4900e984ccf
Abraham Knight <Knighted4GOD@gmail.com>
2022-10-03 16:08:03 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,14 @@
 #include "main.h"
 
 /**
-* _strdup(char *str)
+* _strdup - function that returns a pointer to a newly allocated space in memory
+*space in memory, which contains a copy of the string given as a parameter
+*@str:string to be copied
+*
+* Return: 0
+*/
+
+char *_strdup(char *str)
 {
 	char *cpy;
 	int index, len;
