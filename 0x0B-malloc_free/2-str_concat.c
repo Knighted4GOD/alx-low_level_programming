9b0d3ee6c5839c8622d9f122a8c690dbd78da900
Abraham Knight <Knighted4GOD@gmail.com>
2022-10-04 09:04:49 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,5 @@
 #include "main.h"
-
-
+#include <stddef.h>
 
 /**
  *
@@ -11,7 +10,7 @@
  *
  */
 
-har *str_concat(char *s1, char *s2)
+char *str_concat(char *s1, char *s2)
 
 {
 	char *concat_str;
