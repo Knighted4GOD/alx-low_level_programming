b968aca669efe67894762ce78cd55cffb048eaa2
Abraham Knight <Knighted4GOD@gmail.com>
2022-10-04 07:10:29 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,7 +20,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0 || str == NULL)
 		return (NULL);
 
-	for (i = 0l i < size; i++)
+	for (i = l i < size; i++)
 		str[i] = c;
 	return (str);
 }
