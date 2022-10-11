a4509cf4187157e5f122a8636b9ee714b697dfd6
Abraham Knight <Knighted4GOD@gmail.com>
2022-10-11 12:08:47 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdio.h>
 
 /**
  *main - Prints the name of the file of the program
@@ -9,7 +8,7 @@
  */
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n" ,__FILE__);
 
 	return (0);
 }
