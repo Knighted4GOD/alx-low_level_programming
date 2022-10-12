464bf15aed1f360c477d88b074cd9b37ccaf236e
Abraham Knight <Knighted4GOD@gmail.com>
2022-10-12 10:53:30 -0700

    task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-calc.h"
+
+/**
+ *op_add - returns the sum of 2 nums.
+ *@a: integer
+ *@b: integer
+ *
+ * Return: sun.
+ */
+int op_add(int a, int b)
+{
+	return (a + );
+}
+
+/**
+ * op_sub - return difference of 2 nums
+ * @a: integer
+ * @b: integer
+ *
+ * Return: the difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns product of two nums
+ * @a: integer
+ * @b: integer
+ *
+ * Return: product
+ */
+int op_div(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns the quotient num
+ * @a: integer
+ * @b: integer
+ *
+ * Return: quotient
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - return the remainde of division
+ * @a: integer
+ * @b: integer
+ *
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
