d54fbb62e24cd4b1e473be92e77f87e68e6ee225
Abraham Knight <Knighted4GOD@gmail.com>
2022-10-11 23:37:23 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * print_name - prints a name.
+ * @name: pointer to name.
+ * @f: pointer function.
+ *
+ * Return: 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
