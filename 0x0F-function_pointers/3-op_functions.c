7f0d204ce80ad8e0ffab794f916781b7058ac2ab
Abraham Knight <Knighted4GOD@gmail.com>
2022-10-12 11:14:49 -0700

    task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -5,11 +5,11 @@
  *@a: integer
  *@b: integer
  *
- * Return: sun.
+ * Return: sum.
  */
 int op_add(int a, int b)
 {
-	return (a + );
+	return (a +);
 }
 
 /**
@@ -31,7 +31,7 @@ int op_sub(int a, int b)
  *
  * Return: product
  */
-int op_div(int a, int b)
+int op_mul(int a, int b)
 {
 	return (a * b);
 }
