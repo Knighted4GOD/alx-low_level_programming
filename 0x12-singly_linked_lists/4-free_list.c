589db3ad7ce121d5477dcee9ddb483d134a46869
Abraham Knight <Knighted4GOD@gmail.com>
2022-10-20 14:50:55 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -4,17 +4,20 @@
  * free_list - frees a list
  * @head: head of the linked list
  *
- * Return: no return
+ * Return: 0
  */
 
 void free_list(list_t *head)
 {
-	list_t *current_node;
+	list_t *temp;
 
-	while ((current_node = head) != NULL)
+	while (head)
 	{
-		head = head->nect;
-		free(current_node->str);
-		free(current_node);
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
 	}
+
+	free(head);
 }
