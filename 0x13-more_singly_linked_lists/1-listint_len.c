8cce2c39c3026bf35b1afe989bf8b01f178dee68
Abraham Knight <Knighted4GOD@gmail.com>
2022-10-25 00:17:48 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,11 +1,11 @@
-#include "list.h"
+#include "lists.h"
 #include <stdio.h>
 
 /**
  * listint_len - Returns the num of element in a linked list
  * @h: head of the list
  *
- * Return: num of the elements in 
+ * Return: num of the elements in
  */
 size_t listint_len(const listint_t *h)
 {
