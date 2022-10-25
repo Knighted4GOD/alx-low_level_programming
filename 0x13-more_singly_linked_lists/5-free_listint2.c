21cb4123fcc204cbf6f5600ec86ed298fbb0cdd2
Abraham Knight <Knighted4GOD@gmail.com>
2022-10-25 00:54:14 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lsits.h"
+
+/**
+ * free_listint2 - frees a linstint_t list
+ * @head: pointer to the address
+ *
+ * Return: 0
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+
+	head = NULL;
+}
+			
