30bd3dbf347755bbbe0bbad6d107d393a47f2efd
Abraham Knight <Knighted4GOD@gmail.com>
2022-10-25 00:45:48 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - fress a listint_t list
+ * @head: head of the list
+ *
+ * Return: 0
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		fee(head);
+		head = tmp;
+	}
+
+}
