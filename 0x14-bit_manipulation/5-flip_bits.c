97828d6e40fd174f4d0380225aae35f771bdac39
Abraham Knight <Knighted4GOD@gmail.com>
2022-10-27 08:16:49 -0700

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the num of bites you would need
+ * @n: num 1
+ * @m: num 2
+ *
+ * Return: num of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int nbits;
+
+	for (nbits = 0; n || m; n >>= i, m >> i)
+	{
+		if ((n & 1 != (m & 1))
+				nbits++;
+	}
+
+	return (nbits);
+}
