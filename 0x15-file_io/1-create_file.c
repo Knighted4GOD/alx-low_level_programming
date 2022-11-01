db070058ddb87a011592ed83544e4ec833939c30
Abraham Knight <Knighted4GOD@gmail.com>
2022-11-01 08:00:28 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,7 +2,7 @@
 
 /**
  * create_file - creates a file
- * @filename: A pointer to the name of the file 
+ * @filename: A pointer to the name of the file
  * @text_content: A pointer to a string to write to the file
  *
  * Return: if the function fails - -1.
@@ -30,3 +30,5 @@ int create_file(const char *filename, char *text_content)
 	close(o);
 
 	return (1);
+}
+
